337e6132466e7ec17bceebc6eb74bf64acc6d824
Hyoumori <xx_final_order@yahoo.co.jp>
2017-05-17 11:00:35 +0900

    no message

---
+++ 20170516/D_065.php
@@ -0,0 +1,10 @@
+<?php
+$line = trim(fgets(STDIN));
+if(200 <= $line == $line <= 299){
+    echo "ok";
+}else if(400 <= $line == $line <= 499){
+    echo "error";
+}else{
+    echo "unknown";
+}
+?>
